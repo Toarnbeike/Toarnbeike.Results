1b5ea392fa57101ee10e8c8273b32d5de85a9d17
Toarnbeike <Toarnbeike@gmail.com>
2025-08-01 22:13:20 +0200

    Verify extensions added

---
+++ src/Results/Extensions/VerifyResultTValueExtensions.cs
@@ -0,0 +1,103 @@
+﻿namespace Toarnbeike.Results.Extensions;
+
+/// <summary>
+/// Validate: Quick check on an existing <see cref="Result{TValue}"/> and create a failure if the check fails.
+/// </summary>
+public static class VerifyResultTValueExtensions
+{
+    /// <summary>
+    /// Verifies that the value contained in a successful result satisfies the given <paramref name="predicate"/>.
+    /// </summary>    
+    /// <remarks>
+    /// This method allows enforcing additional invariants on the value inside a successful result.
+    /// If the predicate fails, the result is transformed into a failure using the provided failure factory.
+    /// </remarks>
+    /// <typeparam name="TValue">The type of the value in the result.</typeparam>
+    /// <param name="result">The result to verify.</param>
+    /// <param name="predicate">A predicate that must return <c>true</c> for the value to be considered valid.</param>
+    /// <param name="onFailure">A function that returns a <see cref="Failure"/> if the predicate is <c>false</c>.</param>
+    /// <returns>
+    /// The original result if it was already a failure or the predicate evaluates to <c>true</c>.<br/>
+    /// A new failed result with the provided failure if the predicate evaluates to <c>false</c>>.
+    /// </returns>
+    public static Result<TValue> Verify<TValue>(this Result<TValue> result, Func<TValue, bool> predicate, Func<Failure> onFailure)
+    {
+        if (!result.TryGetValue(out var value))
+        {
+            return result;
+        }
+
+        return predicate(value)
+            ? result
+            : onFailure();
+    }
+
+    /// <summary>
+    /// Verifies that the value contained in a successful result satisfies the given <paramref name="predicate"/>.
+    /// </summary>    
+    /// <remarks>
+    /// This method allows enforcing additional invariants on the value inside a successful result.
+    /// If the predicate fails, the result is transformed into a failure using the provided failure factory.
+    /// </remarks>
+    /// <typeparam name="TValue">The type of the value in the result.</typeparam>
+    /// <param name="result">The result to verify.</param>
+    /// <param name="predicate">A async predicate that must return <c>true</c> for the value to be considered valid.</param>
+    /// <param name="onFailure">A function that returns a <see cref="Failure"/> if the predicate is <c>false</c>.</param>
+    /// <returns>
+    /// The original result if it was already a failure or the predicate evaluates to <c>true</c>.<br/>
+    /// A new failed result with the provided failure if the predicate evaluates to <c>false</c>>.
+    /// </returns>
+    public static async Task<Result<TValue>> VerifyAsync<TValue>(this Result<TValue> result, Func<TValue, Task<bool>> predicate, Func<Failure> onFailure)
+    {
+        if (!result.TryGetValue(out var value))
+        {
+            return result;
+        }
+
+        return await predicate(value)
+            ? result
+            : onFailure();
+    }
+
+    /// <summary>
+    /// Verifies that the value contained in a successful result satisfies the given <paramref name="predicate"/>.
+    /// </summary>    
+    /// <remarks>
+    /// This method allows enforcing additional invariants on the value inside a successful result.
+    /// If the predicate fails, the result is transformed into a failure using the provided failure factory.
+    /// </remarks>
+    /// <typeparam name="TValue">The type of the value in the result.</typeparam>
+    /// <param name="resultTask">The async result to verify.</param>
+    /// <param name="predicate">A predicate that must return <c>true</c> for the value to be considered valid.</param>
+    /// <param name="onFailure">A function that returns a <see cref="Failure"/> if the predicate is <c>false</c>.</param>
+    /// <returns>
+    /// The original result if it was already a failure or the predicate evaluates to <c>true</c>.<br/>
+    /// A new failed result with the provided failure if the predicate evaluates to <c>false</c>>.
+    /// </returns>
+    public static async Task<Result<TValue>> Verify<TValue>(this Task<Result<TValue>> resultTask, Func<TValue, bool> predicate, Func<Failure> onFailure)
+    {
+        var result = await resultTask;
+        return Verify(result, predicate, onFailure);
+    }
+
+    /// <summary>
+    /// Verifies that the value contained in a successful result satisfies the given <paramref name="predicate"/>.
+    /// </summary>    
+    /// <remarks>
+    /// This method allows enforcing additional invariants on the value inside a successful result.
+    /// If the predicate fails, the result is transformed into a failure using the provided failure factory.
+    /// </remarks>
+    /// <typeparam name="TValue">The type of the value in the result.</typeparam>
+    /// <param name="resultTask">The async result to verify.</param>
+    /// <param name="predicate">A async predicate that must return <c>true</c> for the value to be considered valid.</param>
+    /// <param name="onFailure">A function that returns a <see cref="Failure"/> if the predicate is <c>false</c>.</param>
+    /// <returns>
+    /// The original result if it was already a failure or the predicate evaluates to <c>true</c>.<br/>
+    /// A new failed result with the provided failure if the predicate evaluates to <c>false</c>>.
+    /// </returns>
+    public static async Task<Result<TValue>> VerifyAsync<TValue>(this Task<Result<TValue>> resultTask, Func<TValue, Task<bool>> predicate, Func<Failure> onFailure)
+    {
+        var result = await resultTask;
+        return await VerifyAsync(result, predicate, onFailure);
+    }
+}
---
+++ tests/Results.Tests/Extensions/VerifyResultTValueExtensionsTests.cs
@@ -0,0 +1,110 @@
+﻿using Toarnbeike.Results.Extensions;
+using Toarnbeike.Results.Tests.Internal;
+
+namespace Toarnbeike.Results.Tests.Extensions;
+
+/// <summary>
+/// Tests for the <see cref="VerifyResultTValueExtensions"/> class.
+/// </summary>
+public class VerifyResultTValueExtensionsTests
+{
+    private readonly Result<double> _success = Result.Success(1.3);
+    private readonly Result<double> _failure = Result<double>.Failure(new Failure("original", "Original failure"));
+
+    private readonly Task<Result<double>> _successTask = Task.FromResult(Result.Success(1.3));
+    private readonly Task<Result<double>> _failureTask = Task.FromResult(Result<double>.Failure(new Failure("original", "Original failure")));
+
+    private readonly Func<double, bool> _truePredicate = value => value > 1;
+    private readonly Func<double, bool> _falsePredicate = value => value < 1;
+    private readonly Func<double, bool> _forbiddenPredicate = _ => throw new InvalidOperationException("This function should not be called");
+
+    private readonly Func<double, Task<bool>> _trueAsyncPredicate = value => Task.FromResult(value > 1);
+    private readonly Func<double, Task<bool>> _falseAsyncPredicate = value => Task.FromResult(value < 1);
+    private readonly Func<double, Task<bool>> _forbiddenAsyncPredicate = _ => throw new InvalidOperationException("This function should not be called");
+
+    private readonly Func<Failure> _failureFactory = () => new Failure("verify", "Verify failure");
+
+    [Fact]
+    public void Verify_Should_ReturnSuccess_WhenResultIsSuccess_AndPredicateIsTrue()
+    {
+        var result = _success.Verify(_truePredicate, _failureFactory);
+        result.ShouldBeSuccessWithValue(1.3);
+    }
+
+    [Fact]
+    public void Verify_Should_ReturnFailure_WhenResultIsSuccess_AndPredicateIsFalse()
+    {
+        var result = _success.Verify(_falsePredicate, _failureFactory);
+        result.ShouldBeFailureWithCodeAndMessage("verify", "Verify failure");
+    }
+
+    [Fact]
+    public void Verify_Should_ReturnFailure_WhenResultIsFailure()
+    {
+        var result = _failure.Verify(_forbiddenPredicate, _failureFactory);
+        result.ShouldBeFailureWithCodeAndMessage("original", "Original failure");
+    }
+
+    [Fact]
+    public async Task VerifyAsync_Should_ReturnSuccess_WhenResultIsSuccess_AndPredicateIsTrue()
+    {
+        var result = await _success.VerifyAsync(_trueAsyncPredicate, _failureFactory);
+        result.ShouldBeSuccessWithValue(1.3);
+    }
+
+    [Fact]
+    public async Task VerifyAsync_Should_ReturnFailure_WhenResultIsSuccess_AndPredicateIsFalse()
+    {
+        var result = await _success.VerifyAsync(_falseAsyncPredicate, _failureFactory);
+        result.ShouldBeFailureWithCodeAndMessage("verify", "Verify failure");
+    }
+
+    [Fact]
+    public async Task VerifyAsync_Should_ReturnFailure_WhenResultIsFailure()
+    {
+        var result = await _failure.VerifyAsync(_forbiddenAsyncPredicate, _failureFactory);
+        result.ShouldBeFailureWithCodeAndMessage("original", "Original failure");
+    }
+
+    [Fact]
+    public async Task Verify_Should_ReturnSuccess_WhenResultTaskIsSuccess_AndPredicateIsTrue()
+    {
+        var result = await _successTask.Verify(_truePredicate, _failureFactory);
+        result.ShouldBeSuccessWithValue(1.3);
+    }
+
+    [Fact]
+    public async Task Verify_Should_ReturnFailure_WhenResultTaskIsSuccess_AndPredicateIsFalse()
+    {
+        var result = await _successTask.Verify(_falsePredicate, _failureFactory);
+        result.ShouldBeFailureWithCodeAndMessage("verify", "Verify failure");
+    }
+
+    [Fact]
+    public async Task Verify_Should_ReturnFailure_WhenResultTaskIsFailure()
+    {
+        var result = await _failureTask.Verify(_forbiddenPredicate, _failureFactory);
+        result.ShouldBeFailureWithCodeAndMessage("original", "Original failure");
+    }
+
+    [Fact]
+    public async Task VerifyAsync_Should_ReturnSuccess_WhenResultTaskIsSuccess_AndPredicateIsTrue()
+    {
+        var result = await _successTask.VerifyAsync(_trueAsyncPredicate, _failureFactory);
+        result.ShouldBeSuccessWithValue(1.3);
+    }
+
+    [Fact]
+    public async Task VerifyAsync_Should_ReturnFailure_WhenResultTaskIsSuccess_AndPredicateIsFalse()
+    {
+        var result = await _successTask.VerifyAsync(_falseAsyncPredicate, _failureFactory);
+        result.ShouldBeFailureWithCodeAndMessage("verify", "Verify failure");
+    }
+
+    [Fact]
+    public async Task VerifyAsync_Should_ReturnFailure_WhenResultTaskIsFailure()
+    {
+        var result = await _failureTask.VerifyAsync(_forbiddenAsyncPredicate, _failureFactory);
+        result.ShouldBeFailureWithCodeAndMessage("original", "Original failure");
+    }
+}
