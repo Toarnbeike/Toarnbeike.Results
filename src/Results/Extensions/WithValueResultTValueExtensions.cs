b0ad3b2c012536806a4048dd39ed0947b39fb012
Toarnbeike <Toarnbeike@gmail.com>
2025-08-02 09:04:42 +0200

    WithValue extensions added

---
+++ src/Results/Extensions/WithValueResultTValueExtensions.cs
@@ -0,0 +1,124 @@
+﻿namespace Toarnbeike.Results.Extensions;
+
+/// <summary>
+/// WithValue: convert a non-generic result into a typed result by providing a value.
+/// </summary>
+public static class WithValueResultTValueExtensions
+{
+    /// <summary>
+    /// Converts a successful <see cref="Result"/> into a <see cref="Result{T}"/> with the provided <paramref name="value"/>.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value to return in case of success.</typeparam>
+    /// <param name="result">The result to convert.</param>
+    /// <param name="value">The value to include in the result if it was successful.</param>
+    /// <returns>
+    /// A successful result with the provided value if the original result was a success.<br/>
+    /// A failed result with the original failure if the original result was a failure.
+    /// </returns>
+    /// <remarks>
+    /// This method is useful for supplying a value after a non-generic result has already confirmed success.
+    /// </remarks>
+    public static Result<TValue> WithValue<TValue>(this Result result, TValue value)
+    {
+        return result.TryGetFailure(out var failure)
+            ? Result<TValue>.Failure(failure)
+            : Result.Success(value);
+    }
+
+    /// <summary>
+    /// Converts a successful <see cref="Result"/> into a <see cref="Result{T}"/> with the provided <paramref name="value"/>.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value to return in case of success.</typeparam>
+    /// <param name="result">The result to convert.</param>
+    /// <param name="valueFunc">Func to generate the value to include in the result if it was successful.</param>
+    /// <returns>
+    /// A successful result with the provided value if the original result was a success.<br/>
+    /// A failed result with the original failure if the original result was a failure.
+    /// </returns>
+    /// <remarks>
+    /// This method is useful for supplying a value after a non-generic result has already confirmed success.
+    /// </remarks>
+    public static Result<TValue> WithValue<TValue>(this Result result, Func<TValue> valueFunc)
+    {
+        return result.TryGetFailure(out var failure)
+            ? Result<TValue>.Failure(failure)
+            : Result.Success(valueFunc());
+    }
+
+    /// <summary>
+    /// Converts a successful <see cref="Result"/> into a <see cref="Result{T}"/> with the provided <paramref name="value"/>.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value to return in case of success.</typeparam>
+    /// <param name="result">The result to convert.</param>
+    /// <param name="valueFunc">Async func to generate the value to include in the result if it was successful.</param>
+    /// <returns>
+    /// A successful result with the provided value if the original result was a success.<br/>
+    /// A failed result with the original failure if the original result was a failure.
+    /// </returns>
+    /// <remarks>
+    /// This method is useful for supplying a value after a non-generic result has already confirmed success.
+    /// </remarks>
+    public static async Task<Result<TValue>> WithValueAsync<TValue>(this Result result, Func<Task<TValue>> valueFunc)
+    {
+        return result.TryGetFailure(out var failure)
+            ? Result<TValue>.Failure(failure)
+            : Result.Success(await valueFunc());
+    }
+
+    /// <summary>
+    /// Converts a successful <see cref="Result"/> into a <see cref="Result{T}"/> with the provided <paramref name="value"/>.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value to return in case of success.</typeparam>
+    /// <param name="resultTask">The async result to convert.</param>
+    /// <param name="value">The value to include in the result if it was successful.</param>
+    /// <returns>
+    /// A successful result with the provided value if the original result was a success.<br/>
+    /// A failed result with the original failure if the original result was a failure.
+    /// </returns>
+    /// <remarks>
+    /// This method is useful for supplying a value after a non-generic result has already confirmed success.
+    /// </remarks>
+    public static async Task<Result<TValue>> WithValue<TValue>(this Task<Result> resultTask, TValue value)
+    {
+        var result = await resultTask.ConfigureAwait(false);
+        return WithValue(result, value);
+    }
+
+    /// <summary>
+    /// Converts a successful <see cref="Result"/> into a <see cref="Result{T}"/> with the provided <paramref name="value"/>.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value to return in case of success.</typeparam>
+    /// <param name="resultTask">The async result to convert.</param>
+    /// <param name="valueFunc">Func to generate the value to include in the result if it was successful.</param>
+    /// <returns>
+    /// A successful result with the provided value if the original result was a success.<br/>
+    /// A failed result with the original failure if the original result was a failure.
+    /// </returns>
+    /// <remarks>
+    /// This method is useful for supplying a value after a non-generic result has already confirmed success.
+    /// </remarks>
+    public static async Task<Result<TValue>> WithValue<TValue>(this Task<Result> resultTask, Func<TValue> valueFunc)
+    {
+        var result = await resultTask.ConfigureAwait(false);
+        return WithValue(result, valueFunc);
+    }
+
+    /// <summary>
+    /// Converts a successful <see cref="Result"/> into a <see cref="Result{T}"/> with the provided <paramref name="value"/>.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value to return in case of success.</typeparam>
+    /// <param name="resultTask">The async result to convert.</param>
+    /// <param name="valueFunc">Async func to generate the value to include in the result if it was successful.</param>
+    /// <returns>
+    /// A successful result with the provided value if the original result was a success.<br/>
+    /// A failed result with the original failure if the original result was a failure.
+    /// </returns>
+    /// <remarks>
+    /// This method is useful for supplying a value after a non-generic result has already confirmed success.
+    /// </remarks>
+    public static async Task<Result<TValue>> WithValueAsync<TValue>(this Task<Result> resultTask, Func<Task<TValue>> valueFunc)
+    {
+        var result = await resultTask.ConfigureAwait(false);
+        return await WithValueAsync(result, valueFunc);
+    }
+}
---
+++ tests/Results.Tests/Extensions/WithValueExtensionsTests.cs
@@ -0,0 +1,106 @@
+﻿using Toarnbeike.Results.Extensions;
+using Toarnbeike.Results.Tests.Internal;
+
+namespace Toarnbeike.Results.Tests.Extensions;
+
+/// <summary>
+/// Tests for the <see cref="WithValueResultTValueExtensions"/> class.
+/// </summary>
+public class WithValueExtensionsTests
+{
+    private readonly Result _success = Result.Success();
+    private readonly Result _failure = Result.Failure(new Failure("original", "Original failure"));
+
+    private readonly Task<Result> _successTask = Task.FromResult(Result.Success());
+    private readonly Task<Result> _failureTask = Task.FromResult(Result.Failure(new Failure("original", "Original failure")));
+
+    private readonly string _value = "WithValue";
+    private readonly Func<string> _valueFunc = () => "WithValue";
+    private readonly Func<string> _forbiddenFunc = () => throw new InvalidOperationException("This function should not be called");
+    private readonly Func<Task<string>> _valueFuncAsync = () => Task.FromResult("WithValue");
+    private readonly Func<Task<string>> _forbiddenFuncAsync = () => throw new InvalidOperationException("This function should not be called");
+
+    [Fact]
+    public void WithValue_Should_ReturnSuccessFromValue_WhenResultIsSuccess()
+    {
+        var result = _success.WithValue(_value);
+        result.ShouldBeSuccessWithValue("WithValue");
+    }
+
+    [Fact]
+    public void WithValue_Should_ReturnFailureFromValue_WhenResultIsFailure()
+    {
+        var result = _failure.WithValue(_value);
+        result.ShouldBeFailureWithCodeAndMessage("original", "Original failure");
+    }
+
+    [Fact]
+    public void WithValue_Should_ReturnSuccessFromValueFunc_WhenResultIsSuccess()
+    {
+        var result = _success.WithValue(_valueFunc);
+        result.ShouldBeSuccessWithValue("WithValue");
+    }
+
+    [Fact]
+    public void WithValue_Should_ReturnFailureFromValueFunc_WhenResultIsFailure()
+    {
+        var result = _failure.WithValue(_forbiddenFunc);
+        result.ShouldBeFailureWithCodeAndMessage("original", "Original failure");
+    }
+
+    [Fact]
+    public async Task WithValueAsync_Should_ReturnSuccess_WhenResultIsSuccess()
+    {
+        var result = await _success.WithValueAsync(_valueFuncAsync);
+        result.ShouldBeSuccessWithValue("WithValue");
+    }
+
+    [Fact]
+    public async Task WithValueAsync_Should_ReturnFailure_WhenResultIsFailure()
+    {
+        var result = await _failure.WithValueAsync(_forbiddenFuncAsync);
+        result.ShouldBeFailureWithCodeAndMessage("original", "Original failure");
+    }
+
+    [Fact]
+    public async Task WithValue_Should_ReturnSuccessFromValue_WhenResultTaskIsSuccess()
+    {
+        var result = await _successTask.WithValue(_value);
+        result.ShouldBeSuccessWithValue("WithValue");
+    }
+
+    [Fact]
+    public async Task WithValue_Should_ReturnFailureFromValue_WhenResultTaskIsFailure()
+    {
+        var result = await _failureTask.WithValue(_value);
+        result.ShouldBeFailureWithCodeAndMessage("original", "Original failure");
+    }
+
+    [Fact]
+    public async Task WithValue_Should_ReturnSuccessFromValueFunc_WhenResultTaskIsSuccess()
+    {
+        var result = await _successTask.WithValue(_valueFunc);
+        result.ShouldBeSuccessWithValue("WithValue");
+    }
+
+    [Fact]
+    public async Task WithValue_Should_ReturnFailureFromValueFunc_WhenResultTaskIsFailure()
+    {
+        var result = await _failureTask.WithValue(_forbiddenFunc);
+        result.ShouldBeFailureWithCodeAndMessage("original", "Original failure");
+    }
+
+    [Fact]
+    public async Task WithValueAsync_Should_ReturnSuccess_WhenResultTaskIsSuccess()
+    {
+        var result = await _successTask.WithValueAsync(_valueFuncAsync);
+        result.ShouldBeSuccessWithValue("WithValue");
+    }
+
+    [Fact]
+    public async Task WithValueAsync_Should_ReturnFailure_WhenResultTaskIsFailure()
+    {
+        var result = await _failureTask.WithValueAsync(_forbiddenFuncAsync);
+        result.ShouldBeFailureWithCodeAndMessage("original", "Original failure");
+    }
+}
