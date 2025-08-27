5f387d3b016823e4e0ff9e5a764e2aff964ba774
Toarnbeike <toarnbeike@outlook.com>
2025-08-27 19:41:00 +0200

    Integration test for notification handling

---
+++ tests/Results.Integration.Tests/Examples/Notifications.cs
@@ -0,0 +1,14 @@
+﻿using Toarnbeike.Results.Messaging.Notifications;
+
+namespace Toarnbeike.Results.Integration.Tests.Examples;
+
+public record CustomerCreatedNotification(int CustomerId) : NotificationBase("TestSender");
+
+public sealed class CreatedCustomerLoggerHandler(List<string> log) : INotificationHandler<CustomerCreatedNotification>
+{
+    public Task HandleAsync(CustomerCreatedNotification notification, CancellationToken cancellationToken = default)
+    {
+        log.Add($"Customer with Id {notification.CustomerId} created.");
+        return Task.CompletedTask;
+    }
+}
