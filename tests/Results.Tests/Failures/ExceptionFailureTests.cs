c02a720da6f4750c1a462f61322729afa4e15f18
Toarnbeike <toarnbeike@outlook.com>
2026-04-15 23:03:55 +0200

    Added missing failure tests

--- tests/Results.Tests/Failures/ExceptionFailureTests.cs
+++ tests/Results.Tests/Failures/ExceptionFailureTests.cs
@@ -28,3 +28,28 @@ public class ExceptionFailureTests
         newFailure.Message.ShouldBe("Something else");
     }
 }
+
+/// <summary>
+/// Tests for the <see cref="ExceptionFailure"/> record.
+/// </summary>
+public class SimpleFailureTests
+{
+    [Test]
+    public void SimpleFailure_Should_BeCreatedWithCodeAndMessage()
+    {
+        var failure = new SimpleFailure("Code", "Message");
+
+        failure.Code.ShouldBe("Code");
+        failure.Message.ShouldBe("Message");
+        failure.Category.ShouldBe(FailureCategory.Unknown);
+    }
+
+    [Test]
+    public void ExceptionFailure_Should_BeAbleToChangeBaseProperties_UsingWithSyntax()
+    {
+        var failure = new SimpleFailure("Code", "Message");
+
+        var newFailure = failure with { Message = "Something else" };
+        newFailure.Message.ShouldBe("Something else");
+    }
+}
